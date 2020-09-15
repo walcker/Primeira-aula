9bbeb2da04aa8bc78e627d6d919ff64dce0e13c1
Walcker Gomes <walckergomes@gmail.com>
2020-09-15 14:32:24 +0000

    Initial commit

---
+++ retangulo.cpp
@@ -0,0 +1,24 @@
+#include "retangulo.hpp"
+
+int Retangulo::area() {
+	return altura*largura;
+}
+
+int Retangulo::perimetro() {
+	return (2*largura + 2*altura);
+}
+
+int Retangulo::getAltura() {
+	return altura;
+}
+
+void Retangulo::setAltura(int altura_) {
+	altura = altura_;
+}
+
+int Retangulo::getLargura() {
+	return largura;
+}
+void Retangulo::setLargura(int largura_) {
+	largura = largura_;
+}
